c8a9becd1df67aff6491c225714c5efa02d993da
icp1481 <icp1481@naver.com>
2023-07-27 02:47:16 +0900

    songPlay

--- Assets/Scripts/Seonkyu_code/TextSongTitle.cs
+++ Assets/Scripts/Seonkyu_code/TextSongTitle.cs
@@ -9,18 +9,50 @@ public class TextSongTitle : MonoBehaviour
 {
     // Start is called before the first frame update
     public TMP_Text SongTitleText;
+    public List<string> buttonNamesList = new List<string>();
+    public int songIndex;
     /*void Start()
     {
           SongTitleText.text = "What kind of song's rhythm do you want to feel?";  
     }*/
 
-    public void OnClickButtonA() {
-        Debug.Log("Click A");
-        SongTitleText.text = "Feel My Rhythm";
+    void Start()
+    {
+        buttonNamesList.Add("Feel My Rhythm");
+        buttonNamesList.Add("Bad Habits");
+        buttonNamesList.Add("Bang Bang Bang");
+        buttonNamesList.Add("Attention");
+        buttonNamesList.Add("Sugar");
+        buttonNamesList.Add("I Don't Think That I Like Her");
     }
-    public void OnClickButtonOther() {
-        Debug.Log("Click B");
-        SongTitleText.text = "other Song";
+
+    public void OnClickButtonA() {
+        Debug.Log("A");
+        SongTitleText.text = "Feel My Rhythm";
+        MusicPlayer musicPlayer = GetComponent<MusicPlayer>();
+        musicPlayer.PlaySong("FeelMyRhythm");
+    }
+    public void OnClickButtonB() {
+        Debug.Log("B");
+        SongTitleText.text = "Bad Habits";
+        MusicPlayer musicPlayer = GetComponent<MusicPlayer>();
+        musicPlayer.PlaySong("BadHabits");
+    }
+
+    public void OnClickButtonC() {
+        Debug.Log("C");
+        SongTitleText.text = buttonNamesList[2];
+    }
+    public void OnClickButtonD() {
+        Debug.Log("D");
+        SongTitleText.text = buttonNamesList[3];
+    }
+
+    public void OnClickButtonE() {
+        SongTitleText.text = buttonNamesList[4];
+    }
+    public void OnClickButtonF() {
+        SongTitleText.text = buttonNamesList[5];
     }
     // Update is called once per frame
 }
